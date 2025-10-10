03e97acaa796dae47f3ca29d0486108391502d17
NTOable <pardsftw@gmail.com>
2025-10-10 23:34:00 +0800

    Push 11:33 P.M.

--- app/src/main/java/com/example/retail/sample_retail.kt
+++ app/src/main/java/com/example/retail/sample_retail.kt
@@ -1,8 +1,7 @@
 package com.example.retail
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,34 +20,17 @@ class sample_retail : AppCompatActivity() {
         }
 
         // Get IDs
-        val drink_spinner: Spinner=findViewById<Spinner>(R.id.drink_spin)
-        val size_spinner: Spinner=findViewById<Spinner>(R.id.size_spin)
+        val drink_spinner: Spinner = findViewById(R.id.drink_spin)
+        val size_spinner: Spinner = findViewById(R.id.size_spin)
+        val compute_btn = findViewById<Button>(R.id.btn_compute)
+        val show_total = findViewById<TextView>(R.id.show_total)
 
         /*Simple_Retail flow
-        Select Drink (mango, milktea, Choco)
-        ↓
+        Select Drink (mango, milk tea, Choco)
         Size (small, medium , large)
-        ↓
         subtotal
-        ↓
         payment
-        ↓
         Final total/change*/
-
-        // Values for drinks
-        data class Drinks(val name: String, val price: Double)
-        val drinkArray = arrayOf(
-            Drinks("Mango Juice", 20.00),
-            Drinks("Milk Tea", 30.00),
-            Drinks("Milkshake", 40.00)
-        )
-
-            // convert to String
-                val choice_drink = getItemAtPosition(.toString()
-
-        // switch cases for prices
-        // assign an id for each size
-        // Small = S, Medium = M, Large = L
 
         // For Display
         val display_drink = arrayOf("Mango", "Milk Tea", "Milkshake")
@@ -64,6 +46,40 @@ class sample_retail : AppCompatActivity() {
 
                 // to attach
         drink_spinner.adapter = drink_adapter
-        size_spinner.adapter = size_adapter;
+        size_spinner.adapter = size_adapter
+
+        // Price handling
+            // Values for drinks
+        data class Drinks(val name: String, val price: Double)
+            val Fruit1 = Drinks("Mango Juice", 20.00)
+            val Tea1 = Drinks("Milk Tea", 30.00)
+            val Milk1 = Drinks("Milkshake", 40.00)
+
+            var drink_price: Double
+
+            compute_btn.setOnClickListener{
+
+                    val choice_drink = drink_spinner.getSelectedItem()
+                    val choice_size = size_spinner.getSelectedItem()
+
+                    when (choice_size) {
+                        "Small" -> when (choice_drink) {
+                            "Mango Juice" -> drink_price = Fruit1.price * 1
+                            "Milk Tea" -> drink_price = Tea1.price * 1
+                            "Milkshake" -> drink_price = Milk1.price * 1
+                        }
+                        "Medium" -> when (choice_drink) {
+                            "Mango Juice" -> drink_price = Fruit1.price * .25
+                            "Milk Tea" -> drink_price = Tea1.price * .25
+                            "Milkshake" -> drink_price = Milk1.price * .25
+                        }
+                        "Large" -> when (choice_drink) {
+                            "Mango Juice" -> drink_price = Fruit1.price * .5
+                            "Milk Tea" -> drink_price = Tea1.price * .5
+                            "Milkshake" -> drink_price = Milk1.price * .5
+                        }
+                }
+                    show_total.text = drink_price
+             }
+        }
     }
-}
